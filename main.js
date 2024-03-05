fc60b558ee747076706aa2ee29eaa48133f1e496
laurecias <159141191+laurecias@users.noreply.github.com>
2024-03-05 09:04:28 -0300

    .

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ let nomeUsuario = prompt('como você está?');
 let anterior = document.querySelector('#sexo');
 
 anterior.textContent = nomeUsuario; 
+
+while(nomeUsuario == ""){
+    nomeUsuario = prompt('como você está?')
+}
