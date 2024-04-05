22bea1e327fbfa705483f7ea16097c70459117ca
laurecias <159141191+laurecias@users.noreply.github.com>
2024-04-05 12:23:06 -0300

    .

--- cont.js
+++ cont.js
@@ -4,6 +4,14 @@ const paulo = document.querySelectorAll('.botaos');
 for (let x = 0; x<paulo.length; x++){
     console.log(x);
     paulo[x].onclick = function() {
+    for(let y = 0; y<paulo.length; y++){
+     paulo[y].classList.remove('ativo');
+    
+    }
+    paulo[x].classList.add
+
+
+
      paulo[x].classList.add('ativo');
     }
 }
