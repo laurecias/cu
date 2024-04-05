e184f241f98f8f33b4824299a9846fa5d7188eaf
laurecias <159141191+laurecias@users.noreply.github.com>
2024-04-05 12:15:44 -0300

    .

--- cont.js
+++ cont.js
@@ -4,7 +4,6 @@ const paulo = document.querySelectorAll('.botaos');
 for (let x = 0; x<paulo.length; x++){
     console.log(x);
     botoes[x].onclick = function() {
-
      botoes[x].classList.add('ativo');
     }
 }
